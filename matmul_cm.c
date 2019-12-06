255dcc6cb4aeb610972f0cf4148313208a836b39
Jack Lloyd <jack.lloyd@fortanix.com>
2019-12-06 15:10:08 -0500

    remove dead code

--- matmul_cm.c
+++ matmul_cm.c
@@ -113,33 +113,16 @@ void create_secret_matrix_r_t(int16_t r_t[PARAMS_M_BAR][PARAMS_D], const uint8_t
     }
 }
 
-#if !defined(AVX2)
-
 // B = A * S
-
-#if PARAMS_TAU == 0
 
 void matmul_as_q(modq_t d[PARAMS_D][PARAMS_N_BAR], modq_t a[PARAMS_D][PARAMS_D], int16_t s_t[PARAMS_N_BAR][PARAMS_D]) {
 
-#elif PARAMS_TAU == 1
-
-void matmul_as_q(modq_t d[PARAMS_D][PARAMS_N_BAR], modq_t a[2 * PARAMS_D * PARAMS_D], uint32_t a_permutation[PARAMS_D], int16_t s_t[PARAMS_N_BAR][PARAMS_D]) {
-
-#else
-
-void matmul_as_q(modq_t d[PARAMS_D][PARAMS_N_BAR], modq_t a[PARAMS_TAU2_LEN + PARAMS_D], uint16_t a_permutation[PARAMS_D], int16_t s_t[PARAMS_N_BAR][PARAMS_D]) {
-
-#endif
     size_t i, j, l;
 
     // Initialize result
     memset(d, 0, PARAMS_N_BAR * PARAMS_D * sizeof (modq_t));
 
-#if PARAMS_TAU == 0
 #define A_element(j, i) a[j][i]
-#else
-#define A_element(j, i) a[a_permutation[j] + i]
-#endif
     for (j = 0; j < PARAMS_D; j++) {
         for (l = 0; l < PARAMS_N_BAR; l++) {
             for (i = 0; i < PARAMS_D; i++) {
@@ -147,35 +130,18 @@ void matmul_as_q(modq_t d[PARAMS_D][PARAMS_N_BAR], modq_t a[PARAMS_TAU2_LEN + PA
             }
         }
     }
-
-#undef A_element
 }
 
 // U^T = R^T * A
 
-#if PARAMS_TAU == 0
-
 void matmul_rta_q(modq_t d[PARAMS_M_BAR][PARAMS_D], modq_t a[PARAMS_D][PARAMS_D], int16_t r_t[PARAMS_M_BAR][PARAMS_D]) {
 
-#elif PARAMS_TAU == 1
-
-void matmul_rta_q(modq_t d[PARAMS_M_BAR][PARAMS_D], modq_t a[2 * PARAMS_D * PARAMS_D], uint32_t a_permutation[PARAMS_D], int16_t r_t[PARAMS_M_BAR][PARAMS_D]) {
-
-#else
-
-void matmul_rta_q(modq_t d[PARAMS_M_BAR][PARAMS_D], modq_t a[PARAMS_TAU2_LEN + PARAMS_D], uint16_t a_permutation[PARAMS_D], int16_t r_t[PARAMS_M_BAR][PARAMS_D]) {
-
-#endif
     size_t i, j, l;
 
     // Initialize result
     memset(d, 0, PARAMS_M_BAR * PARAMS_D * sizeof (modq_t));
 
-#if PARAMS_TAU == 0
 #define A_element(i, j) a[i][j]
-#else
-#define A_element(i, j) a[a_permutation[i] + j]
-#endif
     for (i = 0; i < PARAMS_D; i++) {
         for (j = 0; j < PARAMS_D; j++) {
             for (l = 0; l < PARAMS_M_BAR; l++) {
@@ -204,9 +170,6 @@ void matmul_stu_p(modp_t d[PARAMS_MU], modp_t u_t[PARAMS_M_BAR][PARAMS_D], int16
         }
     }
 }
-
-
-#endif /* !AVX2 */
 
 // X = B^T * R
 
